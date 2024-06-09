10921a82f864d07ef8c92c687ba9de26c95f3936
Zouine2005 <zouinemohamade@gmail.com>
2024-06-09 20:57:02 +0100

    new modofocation in function

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,53 +1,24 @@
-#!/usr/bin/node
+// Import the axios library for making HTTP requests
+const axios = require('axios');
 
-const request = require('request');
+// URL to get character data from the first film
+const url = 'https://swapi.dev/api/films/1/';
 
-const movieId = process.argv[2];
-const filmEndPoint = 'https://swapi-api.hbtn.io/api/films/' + movieId;
-let people = [];
-const names = [];
-
-const requestCharacters = async () => {
-  await new Promise(resolve => request(filmEndPoint, (err, res, body) => {
-    if (err || res.statusCode !== 200) {
-      console.error('Error: ', err, '| StatusCode: ', res.statusCode);
-    } else {
-      const jsonBody = JSON.parse(body);
-      people = jsonBody.characters;
-      resolve();
-    }
-  }));
-};
-
-const requestNames = async () => {
-  if (people.length > 0) {
-    for (const p of people) {
-      await new Promise(resolve => request(p, (err, res, body) => {
-        if (err || res.statusCode !== 200) {
-          console.error('Error: ', err, '| StatusCode: ', res.statusCode);
-        } else {
-          const jsonBody = JSON.parse(body);
-          names.push(jsonBody.name);
-          resolve();
-        }
-      }));
-    }
-  } else {
-    console.error('Error: Got no Characters for some reason');
-  }
-};
-
-const getCharNames = async () => {
-  await requestCharacters();
-  await requestNames();
-
-  for (const n of names) {
-    if (n === names[names.length - 1]) {
-      process.stdout.write(n);
-    } else {
-      process.stdout.write(n + '\n');
-    }
-  }
-};
-
-getCharNames();
+// Make an HTTP request using axios
+axios.get(url)
+  .then(response => {
+    const data = response.data;
+    console.log('Character names in the film:');
+    data.characters.forEach(characterUrl => {
+      axios.get(characterUrl)
+        .then(characterResponse => {
+          console.log(characterResponse.data.name);
+        })
+        .catch(error => {
+          console.error('Error fetching character data:', error.message);
+        });
+    });
+  })
+  .catch(error => {
+    console.error('Error fetching film data:', error.message);
+  });
